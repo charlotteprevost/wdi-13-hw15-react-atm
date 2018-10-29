3fc7b2595e18a7236dc44395a0494dfbba32762b
Charlotte Prevost <chaprevost91@gmail.com>
2018-10-29 18:45:15 -0500

    Made a depositClick handler function - Logs input

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,14 +2,36 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+
+  constructor(props){
+    super(props);
+    
+    this.userInput = React.createRef();
+    
+    this.state = {
+      balance: 0
+    }
+  }
+
+  depositClick = (event) => {
+  //     event.preventDefault();
+  //     const inputAmount = this.userInput.value;
+  //     const newTotal = this.state.balance + inputAmount;
+  //     this.setState({
+  //       balance: newTotal
+  //     })
+  //     this.userInput.value = null;
+    console.log(this.userInput.value);
+  }
+
   render() {
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
         <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <input ref={input => this.userInput = input} type="text" placeholder="enter an amount" />
+        <input onClick={this.depositClick} type="button" value="Deposit" />
+        <input onClick={this.withdrawClick} type="button" value="Withdraw" />
       </div>
     )
   }
