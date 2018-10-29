b9e3d6542742b324a9e1f91f72799937070b05c1
Charlotte Prevost <chaprevost91@gmail.com>
2018-10-29 18:51:23 -0500

    Fully functional checkings deposit

--- src/Account/index.js
+++ src/Account/index.js
@@ -14,21 +14,26 @@ class Account extends Component {
   }
 
   depositClick = (event) => {
-  //     event.preventDefault();
-  //     const inputAmount = this.userInput.value;
-  //     const newTotal = this.state.balance + inputAmount;
-  //     this.setState({
-  //       balance: newTotal
-  //     })
-  //     this.userInput.value = null;
-    console.log(this.userInput.value);
+      event.preventDefault();
+      const inputAmount = this.userInput.value;
+
+      console.log(`inputAmount: `, inputAmount);
+
+      const newTotal = this.state.balance + parseInt(inputAmount);
+
+      console.log(`newTotal: `, newTotal);
+
+      this.setState({
+        balance: newTotal
+      })
+      // this.userInput.value = null;
   }
 
   render() {
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className="balance">$0</div>
+        <div className="balance">${this.state.balance}</div>
         <input ref={input => this.userInput = input} type="text" placeholder="enter an amount" />
         <input onClick={this.depositClick} type="button" value="Deposit" />
         <input onClick={this.withdrawClick} type="button" value="Withdraw" />
