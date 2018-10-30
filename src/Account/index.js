a128ee4dd4e7ba869b432e85b0ddf372e60ac46d
Charlotte Prevost <chaprevost91@gmail.com>
2018-10-29 20:29:11 -0500

    Can now withdraw only what there is left - no more

--- src/Account/index.js
+++ src/Account/index.js
@@ -13,20 +13,38 @@ class Account extends Component {
     }
   }
 
+  logItAll = () => {
+      console.log(`userInput: `, this.userInput.value);
+      // console.log(`this.inputElem: `, this.inputElem);
+  }
+
+  makeRef = (inputElem) => {
+    this.userInput = inputElem;
+  }
+
   depositClick = (event) => {
       event.preventDefault();
       const inputAmount = this.userInput.value;
-
-      console.log(`inputAmount: `, inputAmount);
-
       const newTotal = this.state.balance + parseInt(inputAmount);
-
-      console.log(`newTotal: `, newTotal);
-
       this.setState({
         balance: newTotal
       })
-      // this.userInput.value = null;
+      this.userInput.value = null;
+  }
+
+  withdrawClick = (event) => {
+      event.preventDefault();
+      let inputAmount = this.userInput.value;
+      if (inputAmount > this.state.balance){
+        inputAmount = this.state.balance
+      } else {
+        inputAmount = this.userInput.value;
+      }
+      const newTotal = this.state.balance - parseInt(inputAmount);
+      this.setState({
+        balance: newTotal
+      })
+      this.userInput.value = null;
   }
 
   render() {
@@ -34,7 +52,7 @@ class Account extends Component {
       <div className="account">
         <h2>{this.props.name}</h2>
         <div className="balance">${this.state.balance}</div>
-        <input ref={input => this.userInput = input} type="text" placeholder="enter an amount" />
+        <input ref={this.makeRef} type="text" placeholder="enter an amount" />
         <input onClick={this.depositClick} type="button" value="Deposit" />
         <input onClick={this.withdrawClick} type="button" value="Withdraw" />
       </div>
